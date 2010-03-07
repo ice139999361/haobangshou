856ad3acccb3b09830ddcb31fcf1f1348258ce42
chnchd001 <chnchd001@ae6af8b6-0899-11df-b3eb-39986f41eb1c>
2010-03-07 03:47:21 +0000

    add code to loginAction

--- hbs/APPLICATION/server/auth/src/com/hbs/auth/contants/AuthConstants.java
+++ hbs/APPLICATION/server/auth/src/com/hbs/auth/contants/AuthConstants.java
@@ -7,4 +7,6 @@ public class AuthConstants {
 	public static final String ROLE_RESOURCE_MANAGER_NAME = "roleResourceMgr";
 	public static final String RESOURCE_MANAGER_NAME = "resourceMgr";
 	public static final String ACTION_MANAGER_NAME = "actionMgr";
+	public static final String LOGIN_MANAGER_NAME = "LoginMgr";
+	public static final String USER_INFO_CACHE_MANAGER_NAME = "UserInfoCacheMgr";
 }
