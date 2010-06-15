5c86fa8a0cd2650ad017653ed9f14ad608684b4c
xuyongfeng78@gmail.com <xuyongfeng78@gmail.com@ae6af8b6-0899-11df-b3eb-39986f41eb1c>
2010-06-15 07:01:31 +0000

    提示中显示全中文

--- trunk/hbs/APPLICATION/server/warehouse/warehousesend/src/com/hbs/warehousesend/action/WarehouseSendUtil.java
+++ trunk/hbs/APPLICATION/server/warehouse/warehousesend/src/com/hbs/warehousesend/action/WarehouseSendUtil.java
@@ -100,16 +100,16 @@ public class WarehouseSendUtil {
 		List<FieldErr> list = new ArrayList<FieldErr>();
 		
 		if(StringUtils.isEmpty(warehouseSend.getCustCode()))
-			list.add(new FieldErr("CustCode","CustCode没有填写"));
+			list.add(new FieldErr("CustCode","客户编码没有填写"));
 		//新的出库单可能存在没有出货单号，出货单号由后台产生的
 //		if(StringUtils.isEmpty(warehouseSend.getSendPoNo()))
 //			list.add(new FieldErr("SendPoNo","SendPoNo没有填写"));
 		if(StringUtils.isEmpty(warehouseSend.getShortName()))
-			list.add(new FieldErr("ShortName","ShortName没有填写"));
+			list.add(new FieldErr("ShortName","客户简称没有填写"));
 		if(warehouseSend.getCreateDate() == null)
-			list.add(new FieldErr("CreateDate","CreateDate没有填写"));
+			list.add(new FieldErr("CreateDate","出库日期没有填写"));
 		if(StringUtils.isEmpty(warehouseSend.getHouseType()))
-			list.add(new FieldErr("HouseType","HouseType没有填写"));
+			list.add(new FieldErr("HouseType","出库仓库没有填写"));
 		if(StringUtils.isEmpty(warehouseSend.getSettlementType()))
 			list.add(new FieldErr("SettlementType","SettlementType没有填写"));
 		if(StringUtils.isEmpty(warehouseSend.getPoNoType()))
@@ -120,15 +120,15 @@ public class WarehouseSendUtil {
 			if(detail == null)
 				continue;
 			if(StringUtils.isEmpty(detail.getCustPartNo()))
-				list.add(new FieldErr("CustPartNo","CustPartNo没有填写"));
+				list.add(new FieldErr("CustPartNo","客户 P/N没有填写"));
 			if(StringUtils.isEmpty(detail.getPartNo()))
-				list.add(new FieldErr("PartNo","PartNo没有填写"));
+				list.add(new FieldErr("PartNo","公司 P/N没有填写"));
 			if(StringUtils.isEmpty(detail.getPnDesc()))
-				list.add(new FieldErr("PnDesc","PnDesc没有填写"));
+				list.add(new FieldErr("PnDesc","物料描述没有填写"));
 			if(detail.getAmount() == null)
-				list.add(new FieldErr("Amount","Amount没有填写"));
+				list.add(new FieldErr("Amount","出库数量没有填写"));
 			if(detail.getTaxRate() == null)
-				list.add(new FieldErr("TaxRate","TaxRate没有填写"));
+				list.add(new FieldErr("TaxRate","税率没有填写"));
 			if(StringUtils.isEmpty(detail.getIsTax()))
 				list.add(new FieldErr("IsTax","IsTax没有填写"));
 		}
